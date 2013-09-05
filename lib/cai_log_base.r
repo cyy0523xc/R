50675292c2cdcfa96870e1b0e2e7e50066b49046
Alex Cai <cyy0523xc@gmail.com>
2013-09-05 18:11:11 +0800

    *

--- lib/cai_log_base.r
+++ lib/cai_log_base.r
@@ -13,6 +13,27 @@ CaiLogBase <- function(filename) {
   class(x) <- "CaiLogBase"
 }
 
+CaiLogBase.load.old <- function(filename) {
+  # LOG文件的数据格式化
+  #   Log文件分行保存，每行的格式例如：aid=29071&ei=866335010754292&mac=2c26c58ac3f2
+  #   说明：&是分隔符，=号左边是字段名，右边是对应的值
+  #
+  # Args:
+  #   filename: log文件地址
+  # Return:
+  #   list
+  
+  # 处理字段名
+  x <- scan(filename, what="", sep="&", nlines=1)
+  fields.name <- sapply(x, FUN=function(x){strsplit(x, split="=")[[1]][1]})
+  names(fields.name) <- NULL
+  names(fields.name) <- fields.name
+  fields.name <- as.list(fields.name)
+  
+  # 读入数据
+  scan(filename, what=fields.name, sep="&")
+}
+
 CaiLogBase.load <- function(filename) {
   # LOG文件的数据格式化
   #   Log文件分行保存，每行的格式例如：aid=29071&ei=866335010754292&mac=2c26c58ac3f2
@@ -26,10 +47,11 @@ CaiLogBase.load <- function(filename) {
   # 处理字段名
   x <- scan(filename, what="", sep="&", nlines=1)
   fields.name <- sapply(x, FUN=function(x){strsplit(x, split="=")[[1]][1]})
-  fields.name <- as.list(tapply(fields.name, fields.name, FUN=function(x){x}))
+  tmp.fields.name <- vector("list", length(fields.name))
+  names(tmp.fields.name) <- fields.name
   
   # 读入数据
-  scan(filename, what=fields.name, sep="&")
+  scan(filename, what=tmp.fields.name, sep="&")
 }
 
 CaiLogBase.one.field.stat <- function(x, p.is.total = T) {
