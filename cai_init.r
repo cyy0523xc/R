c72fa3b01b89598e79d04d10ddb02d9c5d095b6c
Cai Alex <cyy0523xc@gmail.com>
2013-11-26 17:04:01 +0800

    *

--- cai_init.r
+++ cai_init.r
@@ -1,12 +1,15 @@
-CaiInit <- function(p.path="") {
+CaiInit <- function() {
   # 自动加载自己实现的函数库
   # source("/home/windows/Dropbox/code/r/R/cai_init.r")
+  # source("D:/Dropbox/code/r/R/cai_init.r")
   #
   # Args:
   #   p.path: 函数库的目录路径, 例如：D:/Dropbox/code/r/R/lib
   
   # 能否获取当前文件所在的路径？
-  lib.path <- ifelse(p.path == "", "/home/windows/Dropbox/code/r/R/lib/", p.path)
+  lib.path <- ifelse(file.exists('/home/windows/Dropbox/code/r/R/cai_init.r'),
+                     '/home/windows/Dropbox/code/r/R/lib/',
+                     'D:/Dropbox/code/r/R/lib/')
   #setwd(lib.path)
   
   CaiSource <- function(x, p.path) {
